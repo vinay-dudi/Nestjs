658a447cc1963cdb06549315123d122ed0ef54de
unknown <vinay.dudi@divami.com>
2024-03-07 10:42:44 +0530

    Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { NinjasService } from './ninjas/ninjas.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, NinjasService],
 })
 export class AppModule {}
